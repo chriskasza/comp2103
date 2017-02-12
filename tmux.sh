2e239e19a1d6f9067c943b45bdb5e26e9e875613
Chris Kasza <chris.kasza@colibri-software.com>
2017-02-12 13:53:55 -0400

    added a tmux script to root dir

---
+++ tmux.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# directory of assignment currently working on
+wd=a06
+
+# a name for the session
+SESSION=$USER
+
+# start session
+tmux -2 new-session -d -s $SESSION
+
+# change to assignment directory and list files
+tmux send-keys "cd $wd" C-m
+tmux send-keys "ls -la" C-m
+
+# create new pane to right of original (pane 0)
+tmux split-window -h
+tmux send-keys "cd $wd" C-m
+tmux send-keys "ls -la" C-m
+
+# create new pane below pane 1
+tmux split-window -v
+# change to the examples directory and list files
+tmux send-keys "cd c-programs" C-m
+tmux send-keys "ls" C-m
+
+# change focus to pane 0
+tmux select-pane -t 0
+
+# Attach to session
+tmux -2 attach-session -t $SESSION
